7d583b737aa7230c91cff71a7780130860a4751e
Tomas Jerabek <t.jerab@gmail.com>
2015-12-08 11:07:18 +0100

    Zdroje pro modul Clock

--- lekceJavaScript/clock.js
+++ lekceJavaScript/clock.js
@@ -1,37 +1,10 @@
-﻿clock = {
-	time : {h: 0, m: 0, s: 0},
-	actDay : 0,
-	days : ["Nedele", "Pondelí", "Úterý", "Streda", "Ctvrtek", "Pátek", "Sobota"],
-	  
-	setTime : function() {
-		this.d = new Date();
-		this.time.h = this.d.getHours();
-		if(this.time.h < 10) {
-			this.time.h = "0" + this.time.h;
-		}
-		this.time.m = this.d.getMinutes();
-		if(this.time.m < 10) {
-			this.time.m = "0" + this.time.m;
-		}
-		this.time.s = this.d.getSeconds();
-		if(this.time.s < 10) {
-			this.time.s = "0" + this.time.s;
-		}
-		this.actDay = this.d.getDay();
-	},
-	
-	showTime : function() {
-		var a = [this.time.h, this.time.m, this.time.s];
-		document.getElementById("clock").innerHTML = this.days[this.actDay] + ", " + a.join(":");
-	},
-
-	runClock : function() {
-		clearInterval()
-		setInterval(function() {
-			clock.setTime();
-			clock.showTime();
-		}, 1000);
-	}
+var clock = { 
+	digiTime : {h:0, m:0, s:0},
+	actDay : 1,
+	days : ["Neděle", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota"],
+	        
+	setTime : function() {},
+	showTime : function() {},
+	runClock : function() {}
 }
-
 clock.runClock();
